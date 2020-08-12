7d119f1ba1316134f79bcecf914b94d11f9ef4e2
surya <67203625+duckduck-dev@users.noreply.github.com>
2020-08-12 23:12:27 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-      googleClientID: '84073497495-6sjf68pdk3p0igap085ed5b68nuea9o2.apps.googleusercontent.com',
-      googleClientSecret: 'arBI787ngnXBEKjqgaP4DjFv',
-      mongoURI:'mongodb+srv://roughwork:roughwork@roughwork.bebnr.mongodb.net/Demo?retryWrites=true&w=majority',
-      cookieKey: 'jfbsfbkwrbrkwtwjrkt'
-};
